c33e02138a763ff96d27eabb2872899695d96434
Format <format0309@gmail.com>
2021-04-19 22:15:00 +0900

    added "pinrt crawler" line

--- toy-crawler/src/main/java/com/format/app/java/JsoupCrawler.java
+++ toy-crawler/src/main/java/com/format/app/java/JsoupCrawler.java
@@ -10,7 +10,8 @@ public class JsoupCrawler
         String url = "https://www.naver.co.kr";
         Document doc = Jsoup.connect(url).get();
         
-        /** 2. Print Element */ 
+        /** 2. Print Element */
         System.out.println(doc.toString());
+        System.out.println("print crawler");
     }
 }
